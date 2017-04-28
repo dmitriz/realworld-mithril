5b1970593b226b1713ad3e7fc3744798faad5e1d
Barry Els <barryels@gmail.com>
2017-04-28 21:33:46 +0200

    adding initial app header nav

--- src/ui/app/components/AppHeader.js
+++ src/ui/app/components/AppHeader.js
@@ -8,7 +8,12 @@ function view() {
 	return m('header',
 		m('nav.navbar.navbar-light',
 			m('.container',
-				m(Link, {to: '/', className: 'navbar-brand'}, 'conduit')
+				m(Link, { className: 'navbar-brand', to: '/' }, 'conduit'),
+				m('ul.nav.navbar-nav.pull-xs-right', [
+					m('li.nav-item', m(Link, { className: 'nav-link', to: '/' }, 'Home')),
+					m('li.nav-item', m(Link, { className: 'nav-link', to: '/login' }, 'Sign in')),
+					m('li.nav-item', m(Link, { className: 'nav-link', to: '/register' }, 'Sign up'))
+				])
 			)
 		)
 	);

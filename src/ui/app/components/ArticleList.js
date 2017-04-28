e850e0dc1197a5acb1430bdf62babb4fe4fba545
Barry Els <barryels@gmail.com>
2017-04-28 16:35:20 +0200

    adding conditional display logic

--- src/ui/app/components/ArticleList.js
+++ src/ui/app/components/ArticleList.js
@@ -1,8 +1,24 @@
 var m = require('mithril');
 
 
-function view() {
-	return m('div.article-preview', 'No articles are here... yet.');
+var ArticlePreview = require('./ArticlePreview');
+
+
+function view(vnode) {
+	if (!vnode.attrs.articles) {
+		return m('div.article-preview', 'Loading...');
+	}
+
+	if (vnode.attrs.articles.length === 0) {
+		return m('div.article-preview', 'No articles are here... yet.');
+	}
+
+	return m('div',
+		vnode.attrs.articles.map(function (article) {
+			return m(ArticlePreview, { key: article.slug, article: article });
+		})
+		// m('pre', JSON.stringify(vnode.attrs.articles, '', 2))
+	);
 };
 
 

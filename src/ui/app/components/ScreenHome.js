462920e0023f47a0fb0d2ba6eae67a27edd84e51
Barry Els <barryels@gmail.com>
2017-04-28 14:42:25 +0200

    adding grid layout markup and components

--- src/ui/app/components/ScreenHome.js
+++ src/ui/app/components/ScreenHome.js
@@ -2,13 +2,26 @@ var m = require('mithril');
 
 
 var Banner = require('./Banner');
+var ArticleList = require('./ArticleList');
+var FeedToggle = require('./FeedToggle');
+var Tags = require('./Tags');
 
 
 function view() {
 	return m('div.home-page',
 		[
 			m(Banner),
-			m('h1', 'Home')
+			m('.container.page', [
+				m('.row', [
+					m('.col-md-9', [
+						m(FeedToggle),
+						m(ArticleList)
+					]),
+					m('.col-md-3', [
+						m('.sidebar', m(Tags, { type: Tags.types.POPULAR }))
+					])
+				])
+			])
 		]
 	);
 };

dd594a06b0da23a9ae8c1a118730d3b9b74037ce
Barry Els <barryels@gmail.com>
2017-04-28 18:53:31 +0200

    adding initial error list comp

--- src/ui/app/components/ScreenUserLogin.js
+++ src/ui/app/components/ScreenUserLogin.js
@@ -1,7 +1,9 @@
 var m = require('mithril');
 
 
+var domain = require('./../domain');
 var UserLoginForm = require('./UserLoginForm');
+var ListErrors = require('./ListErrors');
 
 
 function view() {
@@ -12,6 +14,7 @@ function view() {
 					m('.col-md-6.offset-md-3.col-xs-12', [
 						m('h1.text-xs-center', 'Sign In'),
 						m('p.text-xs-center', m('a', { href: '#!/register' }, 'Need an account?')),
+						m(ListErrors, {errors: domain.store.userLoginErrors}),
 						m(UserLoginForm)
 					])
 				])
